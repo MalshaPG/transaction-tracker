33c56222b71c7a8f9ce9e4bf09d7e3767a8273b9
Malsha Gamage <malshagamage137@gmail.com>
2025-05-23 10:46:24 +0530

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 type transaction struct {
 	ID          int     `json:"id"`
 	Type        string  `json:"type"`
-	Description string  `json:"name"`
+	Description string  `json:"description"`
 	Date        string  `json:"date"`
 	Amount      float64 `json:"amount"`
 }
