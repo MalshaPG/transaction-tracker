be8d70a44d8a0eeeec5dea50e877a970b780af4c
Malsha Gamage <malshagamage137@gmail.com>
2025-05-25 12:21:32 +0530

    Update main.go to return proper http status, add input validations to saveTransaction function

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+
+	"strings"
 
 	"github.com/go-sql-driver/mysql"
 )
@@ -68,7 +71,7 @@ func getTransactions(c *gin.Context) {
 
 	rows, err := db.Query("SELECT * FROM transactions")
 	if err != nil {
-		c.JSON(500, gin.H{"error": fmt.Sprintf("Query error: %v", err)})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Query error: %v", err)})
 		return
 	}
 	defer rows.Close()
@@ -78,14 +81,14 @@ func getTransactions(c *gin.Context) {
 		var trans transaction
 
 		if err := rows.Scan(&trans.ID, &trans.Type, &trans.Description, &trans.Date, &trans.Amount); err != nil {
-			c.JSON(500, gin.H{"error": fmt.Sprintf("Scan error: %v", err)})
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Scan error: %v", err)})
 			return
 		}
 
 		transactions = append(transactions, trans)
 	}
 	// c.IndentedJSON(http.StatusOK, transactions)
-	c.JSON(200, transactions)
+	c.JSON(http.StatusOK, transactions)
 }
 
 // saveTransaction adds a transaction from JSON received in the request body.
@@ -94,33 +97,54 @@ func saveTransaction(c *gin.Context) {
 	// Call BindJSON to bind the received JSON to newTransaction.
 	var newTransaction transaction
 
+	//Deserialize JSON into a Go struct using BindJSON.
+	// If the JSON is invalid, BindJSON returns an error.
 	if err := c.BindJSON(&newTransaction); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Invalid JSON: %v", err)})
 		return
 	}
 
+	// Validate the transaction data
+	if newTransaction.Type == "" || newTransaction.Amount <= 0 || newTransaction.Date == "" || newTransaction.Description == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "All fields are required and amount must be greater than 0"})
+		return
+	}
+
+	// Validate the transaction type
+	// Convert the transaction type to lowercase to ensure case-insensitivity
+	transactionType := strings.ToLower(newTransaction.Type)
+	if transactionType != "income" && transactionType != "expense" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Transaction type must be 'income' or 'expense'"})
+		return
+	}
+
+	//Overwrite the type
+	newTransaction.Type = transactionType
+
+	// Insert the transaction into the database
 	result, err := db.Exec("INSERT INTO transactions (type, description, date, amount) VALUES (?, ?, ?, ?)", newTransaction.Type, newTransaction.Description, newTransaction.Date, newTransaction.Amount)
 	if err != nil {
-		c.JSON(500, gin.H{"error": fmt.Sprintf("Query error: %v", err)})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Query error: %v", err)})
 		return
 	}
 	id, err := result.LastInsertId()
 	if err != nil {
-		c.JSON(500, gin.H{"error": fmt.Sprintf("Error accessing the ID of the transaction: %v", err)})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Error accessing the ID of the transaction: %v", err)})
 		return
 	}
-	c.JSON(201, gin.H{"id": id})
+	c.JSON(http.StatusCreated, gin.H{"id": id})
 }
 
 // getTransactionByType located the transaction whose type value matches the id
 // paramenter sent by the client, then returns thst tarnsactions as a response
 func getTransactionByType(c *gin.Context) {
-	transactionType := c.Param("type")
+	transactionType := strings.ToLower(c.Param("type"))
 
 	var results []transaction
 
 	rows, err := db.Query("SELECT * FROM transactions WHERE type = ?", transactionType)
 	if err != nil {
-		c.JSON(500, gin.H{"error": fmt.Sprintf("Query error: %v", err)})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Query error: %v", err)})
 		return
 	}
 	defer rows.Close()
@@ -129,18 +153,19 @@ func getTransactionByType(c *gin.Context) {
 	for rows.Next() {
 		var trans transaction
 		if err := rows.Scan(&trans.ID, &trans.Type, &trans.Description, &trans.Date, &trans.Amount); err != nil {
-			c.JSON(500, gin.H{"error": fmt.Sprintf("Scan error: %v", err)})
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Scan error: %v", err)})
 			return
 		}
 		results = append(results, trans)
 	}
 	if err := rows.Err(); err != nil {
-		c.JSON(500, gin.H{"error": fmt.Sprintf("Row error: %v", err)})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Row error: %v", err)})
 	}
 	if len(results) == 0 {
-		c.JSON(404, gin.H{"message": "No transactions found for this type"})
+		c.JSON(http.StatusNotFound, gin.H{"message": "No transactions found for this type"})
 		return
 	}
 
-	c.JSON(200, results)
+	//send json response with the results
+	c.JSON(http.StatusOK, results)
 }
